a8deac1f7d116b39ae81f72c1a5560dc035743be
Sergei Lebedev <superbobry@gmail.com>
2015-08-25 00:31:36 +0300

    Updated ``setup.py`` with metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,50 @@
-from distutils.core import setup
+#! /usr/bin/env python
+
+from setuptools import setup
 from Cython.Build import cythonize
 
-setup(
-    ext_modules = cythonize("benchpy/timed_eval.pyx")
+DISTNAME = "benchpy"
+DESCRIPTION = __doc__
+LONG_DESCRIPTION = open("README.md").read()
+MAINTAINER = "Ann Atamanova"
+MAINTAINER_EMAIL = "anne.atamanova@gmail.com"
+LICENSE = "MIT"
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: MIT License"
+    "Intended Audience :: Developers",
+    "Topic :: Software Development",
+    "Topic :: Scientific/Engineering",
+    "Programming Language :: Cython",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+]
+
+
+import benchpy
+
+VERSION = benchpy.__version__
+
+
+setup_options = dict(
+    name="benchpy",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    license=LICENSE,
+    url="https://github.com/atanna/bm",
+    packages=["benchpy"],
+    classifiers=CLASSIFIERS,
+    ext_modules=cythonize("benchpy/timed_eval.pyx"),
+    requires=["numpy", "scipy", "prettytable"]
 )
 
+
+if __name__ == "__main__":
+    setup(**setup_options)
