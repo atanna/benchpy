5e5054738360c1ce6cd17cda6e16f85e735f181d
Sergei Lebedev <superbobry@gmail.com>
2015-08-25 00:48:08 +0300

    IPython is now not required
      use %%load_ext benchpy to load the extension

--- benchpy/__init__.py
+++ benchpy/__init__.py
@@ -5,4 +5,4 @@ __version__ = "0.1.0"
 
 from .run import bench, group, run
 from .display import plot_results, plot_features, save_info
-from .magic import ip
+from .magic import load_ipython_extension
--- benchpy/magic.py
+++ benchpy/magic.py
@@ -1,127 +1,123 @@
-import pylab as plt
 from functools import partial
-from IPython import get_ipython
-from IPython.core.magic import magics_class, Magics, line_cell_magic
+
+from matplotlib import pyplot as plt
 
 
-@magics_class
-class ExecutionMagics(Magics):
-    @line_cell_magic
-    def benchpy(self, parameter_s='', cell=None):
-        """
-        Run benchpy.run
-        %benchpy [[-i] [-g] [-n <N>] [-m <M>] [-p] [-r <R>] [-t <T>] -s<S>] statement
-        where statement is Bench or Group or list with benches
-        %%benchpy [[-i] -g<G> -m<M> -n<N> [-p] -s<S>]
-          long description of statement
+def magic_benchpy(self, parameter_s='', cell=None):
+    """
+    Run benchpy.run
+    %benchpy [[-i] [-g] [-n <N>] [-m <M>] [-p] [-r <R>] [-t <T>] -s<S>] statement
+    where statement is Bench or Group or list with benches
+    %%benchpy [[-i] -g<G> -m<M> -n<N> [-p] -s<S>]
+      long description of statement
 
 
-        Options:
-        -i: return full information about benchmark results.
+    Options:
+    -i: return full information about benchmark results.
 
-        -g: use information from garbage collector (with_gc=True).
-        Default: 'False'.
+    -g: use information from garbage collector (with_gc=True).
+    Default: 'False'.
 
-        -n<N>: set maximum of batch size <N> (max_batch=<N>).
-        Default: 10.
+    -n<N>: set maximum of batch size <N> (max_batch=<N>).
+    Default: 10.
 
-        -m<M>: set number of batches for fitting regression <M> (n_batches=<M>).
-        Default: 10.
-        batch_sizes = [1, ...,M-2<M>/<N>, M-<M>/<N>, <M>]
+    -m<M>: set number of batches for fitting regression <M> (n_batches=<M>).
+    Default: 10.
+    batch_sizes = [1, ...,M-2<M>/<N>, M-<M>/<N>, <M>]
 
-        -p: show plots with regression.
+    -p: show plots with regression.
 
-        -r<R>: repeat the loop iteration <R> (n_samples=<R>).
-        Default 5.
+    -r<R>: repeat the loop iteration <R> (n_samples=<R>).
+    Default 5.
 
-        -t<T>: choose columns <T> to represent result.
-        <T> = [t][c][f][s][m][M][r][g][i]
-        where
-        t='Time'
-        c='CI' - confidence interval
-        f='Features_time' - time for each regression parameter
-        s='Std' - standard deviation for regression parameter (which means time)
-        m='Min' - minimum of the time values
-        M='Max' - maximum
-        r="R2" - r2 regression score
-        g='gc_time' - time for gc collections (useful only with python version >= 3.3)
-        i='fit_info' - fitting information
+    -t<T>: choose columns <T> to represent result.
+    <T> = [t][c][f][s][m][M][r][g][i]
+    where
+    t='Time'
+    c='CI' - confidence interval
+    f='Features_time' - time for each regression parameter
+    s='Std' - standard deviation for regression parameter (which means time)
+    m='Min' - minimum of the time values
+    M='Max' - maximum
+    r="R2" - r2 regression score
+    g='gc_time' - time for gc collections (useful only with python version >= 3.3)
+    i='fit_info' - fitting information
 
-        Default - default in repr.
+    Default - default in repr.
 
-        Examples
-        --------
-        ::
-        In [1]: import benchpy as bp
+    Examples
+    --------
+    ::
+    In [1]: import benchpy as bp
 
-        In [2]: %benchpy 10**10000
-        +--------------+-------------------------------+-------------------------------+
-        |  Time (µs)   |        CI_tquant[0.95]        |  Features: ['batch' 'const']  |
-        +--------------+-------------------------------+-------------------------------+
-        | 225.33965124 | [ 210.72239262  239.54741751] | [ 177.29140495   48.04824629] |
-        +--------------+-------------------------------+-------------------------------+
+    In [2]: %benchpy 10**10000
+    +--------------+-------------------------------+-------------------------------+
+    |  Time (µs)   |        CI_tquant[0.95]        |  Features: ['batch' 'const']  |
+    +--------------+-------------------------------+-------------------------------+
+    | 225.33965124 | [ 210.72239262  239.54741751] | [ 177.29140495   48.04824629] |
+    +--------------+-------------------------------+-------------------------------+
 
-        In [3]: %benchpy -t tcsrmM 10**10000
-        +---------------+-------------------------------+---------------+----------------+---------------+---------------+
-        |   Time (µs)   |        CI_tquant[0.95]        |      Std      |       R2       |      Min      |      Max      |
-        +---------------+-------------------------------+---------------+----------------+---------------+---------------+
-        | 226.600298929 | [ 213.60009798  240.16961693] | 7.00210625405 | 0.999999184569 | 179.693800055 | 226.248999752 |
-        +---------------+-------------------------------+---------------+----------------+---------------+---------------+
+    In [3]: %benchpy -t tcsrmM 10**10000
+    +---------------+-------------------------------+---------------+----------------+---------------+---------------+
+    |   Time (µs)   |        CI_tquant[0.95]        |      Std      |       R2       |      Min      |      Max      |
+    +---------------+-------------------------------+---------------+----------------+---------------+---------------+
+    | 226.600298929 | [ 213.60009798  240.16961693] | 7.00210625405 | 0.999999184569 | 179.693800055 | 226.248999752 |
+    +---------------+-------------------------------+---------------+----------------+---------------+---------------+
 
-        In [4]: def cycles(n):
-           ...:     for i in range(n):
-           ...:         arr = []
-           ...:         arr.append(arr)
-           ...:
+    In [4]: def cycles(n):
+       ...:     for i in range(n):
+       ...:         arr = []
+       ...:         arr.append(arr)
+       ...:
 
-        In [9]: %benchpy -n 1000 cycles(100)
-        +---------------+-----------------------------+-----------------------------+
-        |   Time (µs)   |       CI_tquant[0.95]       | Features: ['batch' 'const'] |
-        +---------------+-----------------------------+-----------------------------+
-        | 23.3943861198 | [  0.          25.96065552] | [ 20.87035101   2.52403511] |
-        +---------------+-----------------------------+-----------------------------+
+    In [9]: %benchpy -n 1000 cycles(100)
+    +---------------+-----------------------------+-----------------------------+
+    |   Time (µs)   |       CI_tquant[0.95]       | Features: ['batch' 'const'] |
+    +---------------+-----------------------------+-----------------------------+
+    | 23.3943861198 | [  0.          25.96065552] | [ 20.87035101   2.52403511] |
+    +---------------+-----------------------------+-----------------------------+
 
-        In [10]: %benchpy -n 1000 -g cycles(100)
-        +--------------+-----------------------------+-----------------------------+---------------+---------------------------+
-        |  Time (µs)   |       CI_tquant[0.95]       | Features: ['batch' 'const'] |    gc_time    | predicted time without gc |
-        +--------------+-----------------------------+-----------------------------+---------------+---------------------------+
-        | 64.256959342 | [  0.          99.92966164] | [ 28.80691753  35.45004181] | 7.67428691294 |        56.582672429       |
-        +--------------+-----------------------------+-----------------------------+---------------+---------------------------+
+    In [10]: %benchpy -n 1000 -g cycles(100)
+    +--------------+-----------------------------+-----------------------------+---------------+---------------------------+
+    |  Time (µs)   |       CI_tquant[0.95]       | Features: ['batch' 'const'] |    gc_time    | predicted time without gc |
+    +--------------+-----------------------------+-----------------------------+---------------+---------------------------+
+    | 64.256959342 | [  0.          99.92966164] | [ 28.80691753  35.45004181] | 7.67428691294 |        56.582672429       |
+    +--------------+-----------------------------+-----------------------------+---------------+---------------------------+
 
-        """
-        opts, arg_str = self.parse_options(parameter_s, 'igm:n:pr:t:',
-                                           list_all=True, posix=False)
-        glob = self.shell.user_ns
-        if cell is not None:
-            arg_str += '\n' + cell
-            arg_str = self.shell.input_transformer_manager.transform_cell(cell)
-        with_gc = 'g' in opts
-        n_samples = int(opts.get('r', [5])[0])
-        max_batch = int(opts.get('n', [10])[0])
-        n_batches = min(int(max_batch), int(opts.get('m', [10])[0]))
-        table_keys = None
-        table_labels = opts.get('t', [None])[0]
-        if table_labels is not None:
-            table_keys = table_labels
-        f = partial(exec, arg_str, glob)
-        from .run import run, bench
-        res = run(bench(f), with_gc=with_gc,
-                  n_samples=n_samples,
-                  n_batches=n_batches,
-                  max_batch=max_batch,
-                  multi=False)
+    """
+    opts, arg_str = self.parse_options(parameter_s, 'igm:n:pr:t:',
+                                       list_all=True, posix=False)
+    glob = self.shell.user_ns
+    if cell is not None:
+        arg_str += '\n' + cell
+        arg_str = self.shell.input_transformer_manager.transform_cell(cell)
+    with_gc = 'g' in opts
+    n_samples = int(opts.get('r', [5])[0])
+    max_batch = int(opts.get('n', [10])[0])
+    n_batches = min(int(max_batch), int(opts.get('m', [10])[0]))
+    table_keys = None
+    table_labels = opts.get('t', [None])[0]
+    if table_labels is not None:
+        table_keys = table_labels
+    f = partial(exec, arg_str, glob)
+    from .run import run, bench
+    res = run(bench(f), with_gc=with_gc,
+              n_samples=n_samples,
+              n_batches=n_batches,
+              max_batch=max_batch,
+              multi=False)
 
-        if 'i' in opts:
-            print(res._repr("Full"))
-        else:
-            print(res._repr(table_keys, with_empty=False))
+    if 'i' in opts:
+        print(res._repr("Full"))
+    else:
+        print(res._repr(table_keys, with_empty=False))
 
-        if 'p' in opts:
-            res.plot()
-            res.plot_features()
-            plt.show()
+    if 'p' in opts:
+        res.plot()
+        res.plot_features()
+        plt.show()
 
 
-ip = get_ipython()
-if ip is not None:
-    ip.register_magics(ExecutionMagics)
+def load_ipython_extension(ip):
+    """API for IPython to recognize this module as an IPython extension."""
+    ip.define_magic("benchpy", magic_benchpy)
